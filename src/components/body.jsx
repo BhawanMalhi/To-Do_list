63d80b3b47de6f67015c6259d9e5c91b9fbdeb13
BhawanMalhi <Bhawanmalhi29@gmail.com>
2024-05-15 13:02:31 -0300

    working to -list item without local storage

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
 function Body() {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleInput(event) {
+    const newValue = event.target.value;
+    setInputData(newValue);
+  }
+  function addBtn() {
+    
+    setItems(prevItems => {
+        return [...prevItems, inputData];
+      });
+      setInputData("");
+  }
+  function deleteItem()
+  {
+
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleInput} type="text" value={inputData} />
+        <button onClick={addBtn}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item</li>
+      <ul>
+          {items.map(a => (
+            <li key={a}>{a} <button onClick={deleteItem}> <span>X</span></button></li>
+          ))}
         </ul>
       </div>
     </div>
